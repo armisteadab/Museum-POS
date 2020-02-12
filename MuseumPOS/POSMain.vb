3400e3bc3f6a96b8bbd55754b4be9de2d5b73f4b
armisteadab <52898845+armisteadab@users.noreply.github.com>
2020-02-11 23:04:27 -0500

    receipt id setting to remain current

--- MuseumPOS/POSMain.vb
+++ MuseumPOS/POSMain.vb
@@ -17,51 +17,36 @@ Public Class POSMain
     End Sub
 
     Private Sub NewReceiptID()
-
-        Dim sqlString As String, AlreadyInTable As Boolean = False
-        Dim sqlConnect As New SqlConnection(), sConnectionString$
-        Dim nReceiptCount As New Integer
+        Dim sqlConnect As New SqlConnection()
+        Dim sConnectionString As String, sqlString As String
 
         sConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\armis\source\repos\MuseumPOS\Museum POS\MuseumPOS\MuseumPOS.mdf;Integrated Security=True;Connect Timeout=30"
 
         sqlConnect.ConnectionString = sConnectionString
-        sqlConnect.Open()
+        Dim cmd As New SqlCommand
+        cmd.CommandType = CommandType.Text
 
-        Dim commandSQL As SqlCommand
-        Dim bAnyRecordsAtAll As Boolean
+        sqlString = "SELECT Max(ReceiptID) as MaxID, Count(ReceiptID) as CountID from Receipt"
+        cmd.CommandText = sqlString
+        cmd.Connection = sqlConnect
 
-        commandSQL = New SqlCommand()
-        commandSQL.CommandType = CommandType.Text
-        commandSQL.CommandText = "SELECT Count(ReceiptID) as CountID from Receipt"
-        commandSQL.Connection = sqlConnect
+        Dim reader As SqlDataReader
+        Dim previousConnectionState As ConnectionState = sqlConnect.State
 
-        Dim reader = commandSQL.ExecuteReader()
-        Try
+        If sqlConnect.State = ConnectionState.Closed Then
+            sqlConnect.Open()
+        End If
+        reader = cmd.ExecuteReader()
+
             If reader.HasRows Then
-                ' nReceiptCount = 0 + reader.Item("CountID")
-                bAnyRecordsAtAll = (nReceiptCount > 0)
-            End If
-        Catch ex As ArgumentException
-
-        End Try
-
-        If bAnyRecordsAtAll Then
-            sqlString = "SELECT Max(ReceiptID) as MaxID, Count(ReceiptID) as CountID from Receipt"
-            commandSQL = New SqlCommand(sqlString, sqlConnect)
-
-            Try
-                If reader.HasRows Then
-                    nReceiptCurrent = 0 + reader.Item("MaxID")
-                    bAnyRecordsAtAll = True
-                End If
-            Catch ex As ArgumentException
-
-            End Try
+            reader.Read()
+            nReceiptCurrent = 0 + reader.Item("MaxID")
         End If
 
         nReceiptCurrent += 1
         Me.ReceiptNumber = (nReceiptCurrent)
         reader.Close()
+
         sqlConnect.Close()
 
     End Sub
